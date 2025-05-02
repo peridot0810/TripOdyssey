634ab47c40a09f157f16c914b95a11f81a000162
200516bb <200516bb@gmail.com>
2025-05-02 17:19:29 +0900

    initial commit

---
+++ src/test/java/com/ssafy/pjt/SsafyTripFinalKimJoonHyeokParkJiHunBongHyenJinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.pjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafyTripFinalKimJoonHyeokParkJiHunBongHyenJinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
